f8a6edf87fa35300b715a5e7c280bf9446d2a3a7
João N. Matos <me@joaonmatos.com>
2025-02-03 23:26:34 +0100

    Add module documentation to make JSR happy

--- src/decimal.ts
+++ src/decimal.ts
@@ -1,3 +1,17 @@
+/**
+ * This module contains an implementation of arbitrary precision floating
+ * point decimal arithmetic.
+ *
+ * @example
+ * ```ts
+ * import { BigDecimal } from "@joaonmatos/decimal";
+ *
+ * BigDecimal.valueOf(2).pow(200).toString(); // "1606938...835301376"
+ *```
+ *
+ * @module
+ */
+
 import { RoundingMode } from "./rounding";
 
 /**
